5d98b7c8dfdc87a12aa0faf39bb0fd40a0a3a4a1
Bryon Smith <bryon@smithauto.us>
2021-07-31 17:48:42 -0500

    remove redux and use context hooks

---
+++ src/contexts/directory/directory.context.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react'
+import directoryItems from './directory-items'
+
+const DirectoryItemsContext = createContext(directoryItems)
+export default DirectoryItemsContext
